ee7dc5baa0b8b5858c50be9f467e1c897340941c
joshua.burkhan <bai21030@byui.edu>
2025-11-21 14:43:30 -0500

    chapter 16 finished V3.2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -107,12 +107,12 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    throw new Error('Database Error: Failed to Delete Invoice.');
   }
 }
 
